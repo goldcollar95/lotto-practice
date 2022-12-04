ea0cf3828f2583481f149696b7b67ec8bb7c910e
goldcollar95 <yjs7298@naver.com>
2022-12-05 00:27:45 +0900

    feat(lotto): LottoRandoms의 총 개수를 리스트로 구현하는 함수

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoNumbers {
@@ -11,5 +12,16 @@ public class LottoNumbers {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
 
         return numbers;
+    }
+
+
+    public List<List<Integer>> LottoRandomNumbers(int price){
+        List<List<Integer>> RandomNumbers = new ArrayList<>();
+        for(int index = 0; index < price; index++){
+
+        List<Integer> Number = LottoRandomNumber();
+        RandomNumbers.add(Number);
+        }
+        return RandomNumbers;
     }
 }
