5795ecfad499bca2d4d81cc7f2898e0ad1108587
goldcollar95 <yjs7298@naver.com>
2022-12-04 21:50:44 +0900

    feat(lotto): 1~45 6자리의 로또 랜덤숫자, UI로직 InputView 클래스 추가

---
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoNumbers {
+
+
+    private void LottoRandomNumber(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
