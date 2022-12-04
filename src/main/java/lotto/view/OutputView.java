8150496f08035532d530a9dc47882927bd6685f2
goldcollar95 <yjs7298@naver.com>
2022-12-04 23:55:25 +0900

    feat(lotto): number + 개를 구매했습니다를 출력하는 메서드

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,9 @@
 package lotto.view;
 
 public class OutputView {
+
+    public void resultCount(int number){
+        int count = number / 1000;
+        System.out.println(count + "개를 구매했습니다.");
+    }
 }
