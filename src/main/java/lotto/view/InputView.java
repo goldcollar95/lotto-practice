07335b5f3b48dfa92d9cba83c7dd299cb6051a40
goldcollar95 <yjs7298@naver.com>
2022-12-05 13:57:17 +0900

    feat(lotto): WinningNumber 입력, type형 리스트 변환

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,9 @@
 package lotto.view;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
@@ -10,5 +13,17 @@ public class InputView {
         int toIntPirce = Integer.parseInt(InputPirce);
 
         return toIntPirce;
+    }
+
+    public List<String> WinningListNumber(List<String> number){
+            String InputWinning = readLine();
+            List<String> toList = TypeToList(InputWinning);
+        return toList;
+    }
+
+    public List<String> TypeToList(String number){
+        String str = "";
+        List<String> list = Arrays.asList(str.split(","));
+        return list;
     }
 }
