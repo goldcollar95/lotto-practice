f31110a9b7e1478f9170988ad4c514d74cbc964a
goldcollar95 <yjs7298@naver.com>
2022-12-06 21:26:58 +0900

    feat(lotto): 로또매칭관련 메서드구현

--- src/main/java/lotto/model/Match.java
+++ src/main/java/lotto/model/Match.java
@@ -2,11 +2,12 @@ package lotto.model;
 
 import lotto.view.InputView;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Match {
+    private static int matchCount = 0;
+    private static int countCheck;
 
     InputView inputView = new InputView();
     public int compareResult(List<Integer> WinningNumber, List<Integer> RandomsNumber){
@@ -24,13 +25,39 @@ public class Match {
         return result;
     }
 
+    public List<Integer> matchingCorrector(List<List<Integer>> lottoNumbers, List<Integer> WinningNumbers){
+        Map<List<Integer>, Integer> map = new HashMap<>();
+        for(List<Integer> lotto: lottoNumbers){
+            countCheck(map, lotto, WinningNumbers);
+            countCheck = 0;
+        }
+        setMatchCount(map);
+        List<Integer> MatchingLotto = new ArrayList<>();
+        for (Map.Entry<List<Integer>, Integer> entry : map.entrySet()){
+            if(entry.getValue().equals(matchCount)){
+                MatchingLotto = entry.getKey();
+                break;
+            }
+        }
+        return MatchingLotto;
+    }
+
+    public static int getMatchCount(){
+        return matchCount;
+    }
+
+    private void setMatchCount(Map<List<Integer>, Integer> map){
+        countCheck = Collections.max(map.values());
+        matchCount = countCheck;
+    }
+
 
     // 3개일치 --> 1개 추가
     public void countCheck(Map<List<Integer>, Integer> map, List<Integer> lotto, List<Integer> winningNumbers) {
             for(Integer match : winningNumbers){
                 if(lotto.contains(match)){
-                    count++;
-                    map.put(lotto, count);
+                    countCheck++;
+                    map.put(lotto, countCheck);
                 }
             }
     }
