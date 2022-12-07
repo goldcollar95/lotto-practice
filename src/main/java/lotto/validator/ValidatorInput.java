9131f57186416e63ac53579ddfc296bc90210fbb
goldcollar95 <yjs7298@naver.com>
2022-12-07 17:29:14 +0900

    feat(lotto): 유효성 검사 틀 부분 만들기

--- src/main/java/lotto/validator/ValidatorInput.java
+++ src/main/java/lotto/validator/ValidatorInput.java
@@ -1,4 +1,17 @@
 package lotto.validator;
 
 public class ValidatorInput {
+
+    public void ValidatorAllSummary(){
+
+    }
+
+
+    public void ValidatorOtherException(){
+
+    }
+
+    public void ValidatorIsEmpty(){
+
+    }
 }
