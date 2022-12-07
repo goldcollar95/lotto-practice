15e7c21fe909d2622f16b9b34bf80c1361b92025
goldcollar95 <yjs7298@naver.com>
2022-12-07 22:01:22 +0900

    feat(lotto):Validator Bonus 기본 틀 구성

--- src/main/java/lotto/validator/ValidatorBonus.java
+++ src/main/java/lotto/validator/ValidatorBonus.java
@@ -2,5 +2,17 @@ package lotto.validator;
 
 public class ValidatorBonus {
 
+    public void ValidatorBonusTwoSummary(){
+
+    }
+
+
+    public void ValidatorBonusLength(){
+
+    }
+
+    public void ValidatorBonusRange(){
+
+    }
 
 }
