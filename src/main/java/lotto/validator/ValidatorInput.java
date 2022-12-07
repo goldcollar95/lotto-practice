d3ab02749b813559157621d7762a1b5ebfb471b8
goldcollar95 <yjs7298@naver.com>
2022-12-07 18:17:20 +0900

    feat(lotto): 문자타입으로 입력시 예외처리

--- src/main/java/lotto/validator/ValidatorInput.java
+++ src/main/java/lotto/validator/ValidatorInput.java
@@ -1,5 +1,7 @@
 package lotto.validator;
 
+import lotto.view.ErrorMessage;
+
 public class ValidatorInput {
 
     public void ValidatorAllSummary(){
@@ -7,11 +9,18 @@ public class ValidatorInput {
     }
 
 
-    public void ValidatorOtherException(){
-
+    public void ValidatorOtherException(String input){
+        if(!input.matches("^[0-9]*$")){
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_STRING.getMessage());
+        }
     }
 
-    public void ValidatorIsEmpty(){
-
+    public void ValidatorIsEmpty(String input){
+        String[] blankMatching = input.split("");
+        for (String split : blankMatching){
+            if(split.equals(" ")){
+                throw new IllegalArgumentException(ErrorMessage.IS_NOT_EMPTY.getMessage());
+            }
+        }
     }
 }
--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -5,7 +5,9 @@ public enum ErrorMessage {
     SIX_NUMBER_POSSBLITY("[ERROR] 6개의 숫자만 입력해 주세요"),
     ONLY_1000_UNIT("[ERROR] 1000원 단위만 가능합니다"),
     SPLIT_INPUT("[ERROR] 쉼표를 기준으로 입력해주세요"),
-    OVERLAP_INPUT("[ERROR] 당첨 번호와 중복된 번호가 있습니다");
+    OVERLAP_INPUT("[ERROR] 당첨 번호와 중복된 번호가 있습니다"),
+    IS_NOT_STRING("[ERROR] 문자를 입력할 수 없습니다."),
+    IS_NOT_EMPTY("[ERROR] 빈칸은 들어올 수 없습니다.");
 
     private final String Message;
 
