1d0fde3fc04161fe5e55d6169cbde680310ce62b
goldcollar95 <yjs7298@naver.com>
2022-12-07 21:56:38 +0900

    feat(lotto): bonus 예외사항 추가처리

--- src/main/java/lotto/view/ErrorMessage.java
+++ src/main/java/lotto/view/ErrorMessage.java
@@ -7,7 +7,9 @@ public enum ErrorMessage {
     INPUT_SPLIT_INPUT("[ERROR] 쉼표를 기준으로 입력해주세요"),
     OVERLAP_INPUT("[ERROR] 당첨 번호와 중복된 번호가 있습니다"),
     INPUT_IS_NOT_STRING("[ERROR] 문자를 입력할 수 없습니다."),
-    INPUT_IS_NOT_EMPTY("[ERROR] 빈칸은 들어올 수 없습니다.");
+    INPUT_IS_NOT_EMPTY("[ERROR] 빈칸은 들어올 수 없습니다."),
+    BONUS_INPUT_RANGE("[ERROR] 1부터 45의 숫자만 가능합니다."),
+    BONUS_INPUT_LENGTH("[ERROR] 보너스의 숫자는 한개만 가능합니다");
 
     private final String Message;
 
